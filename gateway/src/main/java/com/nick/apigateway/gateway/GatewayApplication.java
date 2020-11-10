f4ed36986bb68b2daf56c12ea7b407f9fa5dd4f6
Nick Nguyen <hungnd.iam@gmail.com>
2020-11-11 01:41:57 +0700

    Use zuul instead of gateway

--- gateway/src/main/java/com/nick/apigateway/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/nick/apigateway/gateway/GatewayApplication.java
@@ -3,11 +3,13 @@ package com.nick.apigateway.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.codec.ServerCodecConfigurer;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableZuulProxy
 public class GatewayApplication {
 
     public static void main(String[] args) {
